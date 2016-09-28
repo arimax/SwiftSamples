bd692bb2e0e21aab7ab4e0cc84e43b5230782a45
Sachiko Kajishima <sachiko@kajishima.com>
2016-09-28 20:37:02 +0900

    ScrollViewのサンプル追加

---
+++ ScrollViewTest/ScrollViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollViewTest
+//
+//  Created by Sachiko Kajishima on 2016/09/28.
+//  Copyright © 2016年 Sachiko Kajishima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
